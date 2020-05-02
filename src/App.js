5393132874125c3783a5c9ca01c347cc7f280d5a
purbayanto <romdoni.purbayanto@tiket.com>
2020-05-02 14:24:30 +0700

    add secure route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
@@ -18,6 +18,7 @@ import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
 import { logout } from "./actions/securityActions";
+import SecureRoute from "./securityUtils/SecureRoute";
 
 const jwtToken = localStorage.jwtToken;
 
@@ -55,28 +56,30 @@ class App extends Component {
             {
               //Private Routes
             }
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/addProject" component={AddProject} />
-            <Route
-              exact
-              path="/updateProject/:projectIdentifier"
-              component={UpdateProject}
-            />
-            <Route
-              exact
-              path="/projectBoard/:projectIdentifier"
-              component={ProjectBoard}
-            />
-            <Route
-              exact
-              path="/addProjectTask/:projectIdentifier"
-              component={AddProjectTask}
-            />
-            <Route
-              exact
-              path="/updateProjectTask/:projectIdentifier/:projectSequence"
-              component={UpdateProjectTask}
-            />
+            <Switch>
+              <SecureRoute exact path="/dashboard" component={Dashboard} />
+              <SecureRoute exact path="/addProject" component={AddProject} />
+              <SecureRoute
+                exact
+                path="/updateProject/:projectIdentifier"
+                component={UpdateProject}
+              />
+              <SecureRoute
+                exact
+                path="/projectBoard/:projectIdentifier"
+                component={ProjectBoard}
+              />
+              <SecureRoute
+                exact
+                path="/addProjectTask/:projectIdentifier"
+                component={AddProjectTask}
+              />
+              <SecureRoute
+                exact
+                path="/updateProjectTask/:projectIdentifier/:projectSequence"
+                component={UpdateProjectTask}
+              />
+            </Switch>
           </div>
         </Router>
       </Provider>
